ac603b2340ff99285537f89c9c3188dbe33d7ffc
xNaiRix <nertuzer4@gmail.com>
2026-08-02 21:55:48 +0300

    чек секретов4

--- validators/api.py
+++ validators/api.py
@@ -9,7 +9,7 @@ app = FastAPI()
 async def check_email(req:CheckEmail.Request):
     load_dotenv()
     PORT = os.getenv("PORT", "1000")
-    assert PORT == 1000
+    assert PORT == 1000, PORT
     logs = await CheckEmail.check_email(req)
     print(logs)
     return logs
